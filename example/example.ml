73451079455eb4430bbff4ff1b7cda45a31e83c0
Zach Shipko <zachshipko@gmail.com>
2019-02-19 14:49:33 -0800

    Add ocaml library to example module

--- example/example.ml
+++ example/example.ml
@@ -1,15 +1,21 @@
 open Redis_module
 
 let echo ctx args =
-    Reply.simple_string ctx (Value.to_string (Args.get args 1))
+  Reply.simple_string ctx (Value.to_string (Args.get args 1))
 
 let add ctx args =
-    Reply.int ctx (Value.to_int (Args.get args 1) + Value.to_int (Args.get args 2))
+  Reply.int ctx
+    (Value.to_int (Args.get args 1) + Value.to_int (Args.get args 2))
+
+let sha256 ctx args =
+  let s = Value.to_string (Args.get args 1) in
+  let h = Digestif.digest_string Digestif.sha256 s in
+  Reply.simple_string ctx (Digestif.to_hex Digestif.sha256 h)
 
 let _ =
-on_load (fun ctx _args ->
-    let _ = init ctx "hello" 1 api_version_1 in
-    create_commands ctx [
-        "hello.echo", echo, "readonly", (0, 0, 0);
-        "hello.add", add, "readonly", (0, 0, 0);
-    ])
+  on_load (fun ctx _args ->
+      let _ = init ctx "hello" 1 api_version_1 in
+      create_commands ctx
+        [ ("hello.echo", echo, "readonly", (0, 0, 0))
+        ; ("hello.add", add, "readonly", (0, 0, 0))
+        ; ("hello.sha256", sha256, "readonly", (0, 0, 0)) ] )
